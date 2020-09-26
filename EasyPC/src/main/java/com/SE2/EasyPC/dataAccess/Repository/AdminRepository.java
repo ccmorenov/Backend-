1229141d8918866b0e889e8f282b1c19662cc3f1
Andres Ricardo Perez <riperezro@unal.edu.co>
2020-09-26 16:06:05 -0500

    last 5 repositories

---
+++ EasyPC/src/main/java/com/SE2/EasyPC/dataAccess/Repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.SE2.EasyPC.dataAccess.Repository;
+
+import com.SE2.EasyPC.dataAccess.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    
+}
