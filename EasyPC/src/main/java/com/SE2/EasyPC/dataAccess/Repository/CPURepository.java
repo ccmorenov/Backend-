35f2099b960fbe1f9e79c677487b67fd0978d857
gaperezsa <gaperezsa@unal.edu.co>
2020-09-26 16:01:25 -0500

    5 initial Repositories

---
+++ EasyPC/src/main/java/com/SE2/EasyPC/dataAccess/Repository/CPURepository.java
@@ -0,0 +1,12 @@
+package com.SE2.EasyPC.dataAccess.Repository;
+
+import com.SE2.EasyPC.dataAccess.model.CPU;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CPURepository extends JpaRepository<CPU, Long> {
+
+}
---
+++ EasyPC/src/main/java/com/SE2/EasyPC/dataAccess/Repository/GPURepository.java
@@ -0,0 +1,12 @@
+package com.SE2.EasyPC.dataAccess.Repository;
+
+import com.SE2.EasyPC.dataAccess.model.GPU;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface GPURepository extends JpaRepository<GPU, Long> {
+
+}
