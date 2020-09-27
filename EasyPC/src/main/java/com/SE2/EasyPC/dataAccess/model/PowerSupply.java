5eedb9b25857a7d9a43a656b65b7417177e5c62d
Andres Ricardo Perez <riperezro@unal.edu.co>
2020-09-26 20:46:55 -0500

    fix power supply connection to db

--- EasyPC/src/main/java/com/SE2/EasyPC/dataAccess/model/PowerSupply.java
+++ EasyPC/src/main/java/com/SE2/EasyPC/dataAccess/model/PowerSupply.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Column;
 @Entity
-@Table(name="PowerSupply")
+@Table(name="Powersupply")
 public class PowerSupply {
     @Column(name = "id_powersupply")
     @Id
