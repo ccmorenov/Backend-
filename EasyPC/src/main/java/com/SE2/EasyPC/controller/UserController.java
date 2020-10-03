f299437a8769743de7b6266ab7b5905eb9027f1e
gaperezsa <gaperezsa@unal.edu.co>
2020-10-02 19:45:30 -0500

    Revert "logging for 5 following controller/services"
    This reverts commit a2b5f4d02429daf00ef6d0731ee1d6e801ff4741.

--- EasyPC/src/main/java/com/SE2/EasyPC/controller/UserController.java
+++ EasyPC/src/main/java/com/SE2/EasyPC/controller/UserController.java
@@ -2,14 +2,13 @@ package com.SE2.EasyPC.controller;
 
 import com.SE2.EasyPC.dataAccess.model.User;
 import com.SE2.EasyPC.service.UserService;
-import com.SE2.EasyPC.logging.Log;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @CrossOrigin
@@ -21,26 +20,22 @@ public class UserController {
     UserService userService;
 
     @GetMapping("/users")
-    public List<User> getAllUsers( HttpServletRequest request ) {
-        Log.createLog(0, "getAllUsers query received by " + request.getRemoteAddr());
+    public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/user/{id}")
-    public User getUsersById(@PathVariable(value = "id") Long userId, HttpServletRequest request ) {
-        Log.createLog(0, "getUsersById query received by " + request.getRemoteAddr() );
+    public User getUsersById(@PathVariable(value = "id") Long userId) {
         return userService.getUserById(userId);
     }
 
     @PostMapping("/user")
-    public User createUser(@Valid @RequestBody User user, HttpServletRequest request) {
-        Log.createLog(0, "createUser query received by " + request.getRemoteAddr() );
+    public User createUser(@Valid @RequestBody User user) {
         return userService.createUser(user);
     }
 
     @DeleteMapping("/user/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable(value = "id") Long userId, HttpServletRequest request) {
-        Log.createLog(0, "deleteUser query received by " + request.getRemoteAddr() );
+    public ResponseEntity<?> deleteUser(@PathVariable(value = "id") Long userId) {
         userService.deleteUser(userId);
         return ResponseEntity.ok().build();
     }
--- EasyPC/src/main/java/com/SE2/EasyPC/service/UserService.java
+++ EasyPC/src/main/java/com/SE2/EasyPC/service/UserService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.SE2.EasyPC.dataAccess.model.User;
 import com.SE2.EasyPC.dataAccess.repository.UserRepository;
 import com.SE2.EasyPC.exception.ResourceNotFoundException;
-import com.SE2.EasyPC.logging.Log;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,43 +16,19 @@ public class UserService {
     UserRepository userRepository; 
 
     public List<User> getAllUsers() {
-        try{
-            return userRepository.findAll();
-        }catch( Exception e ){
-            Log.createLog(3, "Service getAllUsers failed: " + e.getMessage() );
-            throw e;
-        }
-        
+        return userRepository.findAll();
     }
 
     public User getUserById( Long id ) {
-        try{
-            return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
-        }catch( Exception e ){
-            Log.createLog(3, "Service getUserById failed: " + e.getMessage() );
-            throw e;
-        }
-        
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
     }
 
     public User createUser(User user) {
-        try{
-            return userRepository.save(user);
-        }catch( Exception e ){
-            Log.createLog(3, "Service createUser failed: " + e.getMessage() );
-            throw e;
-        }
-        
+        return userRepository.save(user);
     }
 
     public void deleteUser( Long id ) {
-        try{
-            User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
-            userRepository.delete(user);
-        }catch( Exception e ){
-            Log.createLog(3, "Service deleteUser failed: " + e.getMessage() );
-            throw e;
-        }
-        
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User", "id", id));
+        userRepository.delete(user);
     }
 }
