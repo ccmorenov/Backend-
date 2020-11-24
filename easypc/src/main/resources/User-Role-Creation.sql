f13721ff2d901df18eb492d0b16d16e6a922ea31
Andres Ricardo Perez <riperezro@unal.edu.co>
2020-11-24 07:35:11 -0500

    update case User

--- easypc/src/main/resources/User-Role-Creation.sql
+++ easypc/src/main/resources/User-Role-Creation.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS `woja38qpioj7dt3x`.`Role` ;
+DROP TABLE IF EXISTS `woja38qpioj7dt3x`.`role` ;
 
-CREATE TABLE IF NOT EXISTS `woja38qpioj7dt3x`.`Role` (
+CREATE TABLE IF NOT EXISTS `woja38qpioj7dt3x`.`role` (
   `id_role` INT NOT NULL,
   `role_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_role`))
@@ -16,15 +16,15 @@ CREATE TABLE IF NOT EXISTS `woja38qpioj7dt3x`.`User_role` (
   INDEX `fk_User_has_Role_User_idx` (`id_user` ASC) VISIBLE,
   CONSTRAINT `fk_User_has_Role_User`
     FOREIGN KEY (`id_user`)
-    REFERENCES `woja38qpioj7dt3x`.`User` (`id_user`)
+    REFERENCES `woja38qpioj7dt3x`.`user` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_User_has_Role_Role1`
     FOREIGN KEY (`id_role`)
-    REFERENCES `woja38qpioj7dt3x`.`Role` (`id_role`)
+    REFERENCES `woja38qpioj7dt3x`.`role` (`id_role`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ;
 
-insert into `woja38qpioj7dt3x`.Role (id_role,role_name) values(1,"client");
-insert into `woja38qpioj7dt3x`.Role (id_role,role_name) values(2,"admin");
+insert into `woja38qpioj7dt3x`.role (id_role,role_name) values(1,"client");
+insert into `woja38qpioj7dt3x`.role (id_role,role_name) values(2,"admin");
