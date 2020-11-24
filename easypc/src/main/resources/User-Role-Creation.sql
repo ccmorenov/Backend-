0f7b1e48d294800c175b1db4debc4ba1272d5cce
Andres Ricardo Perez <riperezro@unal.edu.co>
2020-11-24 07:30:22 -0500

    update DBName

--- easypc/src/main/resources/User-Role-Creation.sql
+++ easypc/src/main/resources/User-Role-Creation.sql
@@ -1,14 +1,14 @@
-DROP TABLE IF EXISTS `easypcunal`.`Role` ;
+DROP TABLE IF EXISTS `woja38qpioj7dt3x`.`Role` ;
 
-CREATE TABLE IF NOT EXISTS `easypcunal`.`Role` (
+CREATE TABLE IF NOT EXISTS `woja38qpioj7dt3x`.`Role` (
   `id_role` INT NOT NULL,
   `role_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_role`))
 ;
 
-DROP TABLE IF EXISTS `easypcunal`.`User_role` ;
+DROP TABLE IF EXISTS `woja38qpioj7dt3x`.`User_role` ;
 
-CREATE TABLE IF NOT EXISTS `easypcunal`.`User_role` (
+CREATE TABLE IF NOT EXISTS `woja38qpioj7dt3x`.`User_role` (
   `id_user` INT NOT NULL,
   `id_role` INT NOT NULL,
   PRIMARY KEY (`id_user`, `id_role`),
@@ -16,15 +16,15 @@ CREATE TABLE IF NOT EXISTS `easypcunal`.`User_role` (
   INDEX `fk_User_has_Role_User_idx` (`id_user` ASC) VISIBLE,
   CONSTRAINT `fk_User_has_Role_User`
     FOREIGN KEY (`id_user`)
-    REFERENCES `easypcunal`.`User` (`id_user`)
+    REFERENCES `woja38qpioj7dt3x`.`User` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_User_has_Role_Role1`
     FOREIGN KEY (`id_role`)
-    REFERENCES `easypcunal`.`Role` (`id_role`)
+    REFERENCES `woja38qpioj7dt3x`.`Role` (`id_role`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ;
 
-insert into `easypcunal`.Role (id_role,role_name) values(1,"client");
-insert into `easypcunal`.Role (id_role,role_name) values(2,"admin");
+insert into `woja38qpioj7dt3x`.Role (id_role,role_name) values(1,"client");
+insert into `woja38qpioj7dt3x`.Role (id_role,role_name) values(2,"admin");
