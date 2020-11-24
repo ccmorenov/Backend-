6de96bdadad26cbf8f7b3db717956cd963af1c52
Andres Ricardo Perez <riperezro@unal.edu.co>
2020-11-23 22:34:33 -0500

    undo case changes user-role-creation

--- easypc/src/main/resources/User-Role-Creation.sql
+++ easypc/src/main/resources/User-Role-Creation.sql
@@ -1,14 +1,14 @@
-DROP TABLE IF EXISTS `heroku_1cd996d0bc8bc84`.`role` ;
+DROP TABLE IF EXISTS `heroku_1cd996d0bc8bc84`.`Role` ;
 
-CREATE TABLE IF NOT EXISTS `heroku_1cd996d0bc8bc84`.`role` (
+CREATE TABLE IF NOT EXISTS `heroku_1cd996d0bc8bc84`.`Role` (
   `id_role` INT NOT NULL,
   `role_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_role`))
 ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `heroku_1cd996d0bc8bc84`.`user_role` ;
+DROP TABLE IF EXISTS `heroku_1cd996d0bc8bc84`.`User_role` ;
 
-CREATE TABLE IF NOT EXISTS `heroku_1cd996d0bc8bc84`.`user_role` (
+CREATE TABLE IF NOT EXISTS `heroku_1cd996d0bc8bc84`.`User_role` (
   `id_user` INT NOT NULL,
   `id_role` INT NOT NULL,
   PRIMARY KEY (`id_user`, `id_role`),
@@ -16,12 +16,12 @@ CREATE TABLE IF NOT EXISTS `heroku_1cd996d0bc8bc84`.`user_role` (
   INDEX `fk_User_has_Role_User_idx` (`id_user` ASC) VISIBLE,
   CONSTRAINT `fk_User_has_Role_User`
     FOREIGN KEY (`id_user`)
-    REFERENCES `heroku_1cd996d0bc8bc84`.`user` (`id_user`)
+    REFERENCES `heroku_1cd996d0bc8bc84`.`User` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_User_has_Role_Role1`
     FOREIGN KEY (`id_role`)
-    REFERENCES `heroku_1cd996d0bc8bc84`.`role` (`id_role`)
+    REFERENCES `heroku_1cd996d0bc8bc84`.`Role` (`id_role`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
